13cade4eb3d1bc72553adf368f1e7ea9459d86de
moiseslugo-04 <moiseslugo9134@gmail.com>
2025-01-05 19:04:27 -0300

    add dist

--- src/components/Button.tsx
+++
@@ -1,6 +0,0 @@
-export default function Button({ children, action = '' }) {
-  const handleClick = () => {
-    console.log(action)
-  }
-  return <button onClick={handleClick}>{children}</button>
-}
